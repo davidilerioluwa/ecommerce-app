0f7e9f24eef9359cf58ece5126654f07224ae1be
David Godswill Ilerioluwa <76020967+davidilerioluwa@users.noreply.github.com>
2023-04-13 01:25:52 +0100

    Update app.js

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
 
 app.use(Session({
-    secret: "helloworld",
+    secret: process.env.secret,
     resave: true,
     saveUninitialized: true,
    
